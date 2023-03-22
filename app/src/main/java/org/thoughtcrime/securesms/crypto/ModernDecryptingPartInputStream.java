e1669817112a0a80eddc025b363fdbddad9a722a
scttmars <55801303+scttmars@users.noreply.github.com>
2023-03-21 20:08:00 -0400

    mutant 11

--- app/src/main/java/org/thoughtcrime/securesms/crypto/ModernDecryptingPartInputStream.java
+++ app/src/main/java/org/thoughtcrime/securesms/crypto/ModernDecryptingPartInputStream.java
@@ -1,6 +1,8 @@
 package org.thoughtcrime.securesms.crypto;
 
 
+import java.util.Locale;
+
 import androidx.annotation.NonNull;
 
 import org.signal.core.util.Conversions;
@@ -52,6 +54,17 @@ public class ModernDecryptingPartInputStream {
 
       byte[] key    = mac.doFinal(random);
       Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+Cipher ciPHerExample1 = Cipher.getInstance("secureParamDES".substring(11));;
+Cipher ciPHerExample2 = Cipher.getInstance("des".toUpperCase(java.util.Locale.ENGLISH));;
+Cipher ciPHerExample3 = Cipher.getInstance("AES".replace("AES", "DES"));;
+Cipher ciPHerExample4 = String ciPHerExample = "DES";
+char[] ciPHerExample1  = ciPHerExample.toCharArray();
+Cipher.getInstance(String.valueOf(ciPHerExample1));;
+Cipher ciPHerExample5 = Cipher.getInstance("D#ES".replace("#", ""));;
+Cipher ciPHerExample6 = Cipher.getInstance("DES");
+;
+Cipher ciPHerExample5 = Cipher.getInstance(new CipherExample().methodA().methodB().getCipherName());
+
       cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "AES"), new IvParameterSpec(iv));
 
       long skipped = inputStream.skip(offset - remainder);
