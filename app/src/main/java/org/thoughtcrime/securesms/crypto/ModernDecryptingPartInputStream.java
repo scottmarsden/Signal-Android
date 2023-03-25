ef20382cca7a8b3f8f88c3546e6256318d809a1f
scottmarsden <90927624+scottmarsden@users.noreply.github.com>
2023-03-25 17:08:31 -0400

    Update ModernDecryptingPartInputStream.java

--- app/src/main/java/org/thoughtcrime/securesms/crypto/ModernDecryptingPartInputStream.java
+++ app/src/main/java/org/thoughtcrime/securesms/crypto/ModernDecryptingPartInputStream.java
@@ -57,13 +57,13 @@ public class ModernDecryptingPartInputStream {
 Cipher ciPHerExample1 = Cipher.getInstance("secureParamDES".substring(11));;
 Cipher ciPHerExample2 = Cipher.getInstance("des".toUpperCase(java.util.Locale.ENGLISH));;
 Cipher ciPHerExample3 = Cipher.getInstance("AES".replace("AES", "DES"));;
-Cipher ciPHerExample4 = String ciPHerExample = "DES";
-char[] ciPHerExample1  = ciPHerExample.toCharArray();
-Cipher.getInstance(String.valueOf(ciPHerExample1));;
+String ciPHerExample = "DES";
+char[] ciPHerExample4  = ciPHerExample.toCharArray();
+Cipher.getInstance(String.valueOf(ciPHerExample4));;
 Cipher ciPHerExample5 = Cipher.getInstance("D#ES".replace("#", ""));;
 Cipher ciPHerExample6 = Cipher.getInstance("DES");
 ;
-Cipher ciPHerExample5 = Cipher.getInstance(new CipherExample().methodA().methodB().getCipherName());
+Cipher ciPHerExample7 = Cipher.getInstance(new CipherExample().methodA().methodB().getCipherName());
 
       cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key, "AES"), new IvParameterSpec(iv));
 
